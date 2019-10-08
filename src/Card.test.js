e68108ff4f4d763141c570903e524648f137e9d9
Glaiza Wagner <glaizawagner@gmail.com>
2019-10-08 09:35:44 -0700

    Card  test snapshot updated

--- src/Card.test.js
+++ src/Card.test.js
@@ -3,15 +3,19 @@ import ReactDOM from 'react-dom';
 import Card from './Card';
 import renderer from 'react-test-renderer';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  
-  ReactDOM.render(<Card />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('App component test', () => {
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    
+    ReactDOM.render(<Card />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('renders the app as expected', () => {
+    const tree = renderer.create(<Card />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
 
-it('renders the app as expected', () => {
-  const tree = renderer.create(<Card />)
-    .toJSON();
-  expect(tree).toMatchSnapshot();
-});
