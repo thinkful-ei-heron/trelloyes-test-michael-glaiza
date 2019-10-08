d5451e2576090c083ccc0cd582287877c6fbf02c
Michael Kirsch <michaelskirsch@gmail.com>
2019-10-08 10:14:45 -0600

    working on snapshot test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
 import App from './App';
+import STORE from './STORE';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders the app as expected', () => {
+  const tree = renderer.create(<App store={STORE}/>)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
 });
